a8ebfa6a01785c10c658ee99184c5114980cbb8b
Korenistov Pavel <pkorenistov@gmail.com>
2026-02-06 15:42:39 +0500

    other changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	app "MaterialsFilter/internal/app"
-	cfg "MaterialsFilter/internal/infrastructure/config"
+	cfg "MaterialsFilter/internal/domain/config"
+	pathFile "MaterialsFilter/internal/infrastructure/path"
 	"log"
 )
 
 func main() {
-	cfg, err := cfg.Load()
+	// инициализация пути для конфигурационного файла
+	path := pathFile.New("..")
+	configPath, err := path.Config()
 	if err != nil {
 		log.Fatal(err)
 	}
-	app.Run(cfg)
+
+	cfg.CheckConfig(configPath) // создание конфигурационного файла, если его нет
+
+	config, err := cfg.NewConfig(configPath) // получение конфигурации
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg.OutputData(config)   // вывод информации о конфигурации
+	cfg.ChangeConfig(config) // изменение конфигурации (добавление, удаление фильтров и т.д.)
+	cfg.SaveConfig(config, configPath) // сохранение конфигурации в файл
+	app.Run(config)
 }
