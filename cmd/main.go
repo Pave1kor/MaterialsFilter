e56e9240a8e9d6ea8e504c6d0a059c5d499bf387
Korenistov Pavel <pkorenistov@gmail.com>
2026-02-09 16:47:03 +0500

    change path methods

--- cmd/main.go
+++ cmd/main.go
@@ -8,22 +8,27 @@ import (
 )
 
 func main() {
-	// инициализация пути для конфигурационного файла
+	// инициализация пути конфигурационного файла
 	path := pathFile.New("..")
 	configPath, err := path.Config()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cfg.CheckConfig(configPath) // создание конфигурационного файла, если его нет
+	// инициализация путей
+	err = path.Path()
+	if err != nil {
+		log.Fatal()
+	}
 
+	cfg.CheckConfig(configPath)              // создание конфигурационного файла, если его нет
 	config, err := cfg.NewConfig(configPath) // получение конфигурации
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cfg.OutputData(config)   // вывод информации о конфигурации
-	cfg.ChangeConfig(config) // изменение конфигурации (добавление, удаление фильтров и т.д.)
+	cfg.ConfigData(config)             // вывод информации о конфигурации
+	cfg.ChangeConfig(config)           // изменение конфигурации (добавление, удаление фильтров и т.д.)
 	cfg.SaveConfig(config, configPath) // сохранение конфигурации в файл
 	app.Run(config)
 }
